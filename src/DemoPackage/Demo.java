d7008ab468b25485dbf58fec4256e0b853aad520
jeyapradeep <jeyapradeep.gopi@gds.ey.com>
2024-04-21 17:58:47 +0530

    first commit

---
+++ src/DemoPackage/Demo.java
@@ -0,0 +1,12 @@
+package DemoPackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
